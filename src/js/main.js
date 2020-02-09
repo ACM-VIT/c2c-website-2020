260dbdf59d0e5ef683d562252cb0b4a9f6c127cd
HelixW <shreyas.2000@hotmail.com>
2020-02-09 13:35:44 +0530

    feat: FAQs now dynamically allocated through the faqpool in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,79 @@
-document.querySelectorAll('.collapsible-header').forEach(btn =>
-  btn.addEventListener('click', () => {
-    const collapsibleContent = btn.nextElementSibling;
-    btn.classList.toggle('active');
+// FAQs
+const FAQ_DATA = [
+  {
+    q: 'Lorem ipsum dolor sit amet, consectetur adipisicing?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta maxime officiis libero eaque ex.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit amet, consectetur adipisicing?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis soluta maxime officiis libero eaque ex.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit amet consectetur?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptate praesentium voluptatem debitis nesciunt officiis ullam animi. Optio.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit amet consectetur?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptate praesentium voluptatem debitis nesciunt officiis ullam animi. Optio.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit?',
+    a:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quidem.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit?',
+    a:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quidem.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit amet consectetur?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptate praesentium voluptatem debitis nesciunt officiis ullam animi. Optio.'
+  },
+  {
+    q: 'Lorem ipsum dolor sit amet consectetur?',
+    a:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptate praesentium voluptatem debitis nesciunt officiis ullam animi. Optio.'
+  },
+  {
+    q: 'Lorem, ipsum dolor?',
+    a:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quidem.'
+  }
+];
 
-    if (btn.classList.contains('active')) {
-      collapsibleContent.style.maxHeight =
-        collapsibleContent.scrollHeight + 'rem';
-    } else {
-      collapsibleContent.style.maxHeight = 0;
-    }
-  })
-);
+let docFrag = document.createDocumentFragment();
+
+window.onload = () => {
+  FAQ_DATA.map(qGroup => {
+    const tempNode = document
+      .querySelector("div[data-type='collapsible-template']")
+      .cloneNode(true);
+    tempNode.querySelector('.question').textContent = qGroup.q;
+    tempNode.querySelector('.collapsible-content p').textContent = qGroup.a;
+    tempNode.style.display = 'block';
+    docFrag.appendChild(tempNode);
+  });
+  document
+    .querySelector('.faq-grid')
+    .insertBefore(docFrag, document.querySelector('.code-of-conduct'));
+  docFrag = null;
+  document.querySelectorAll('.collapsible-header').forEach(btn =>
+    btn.addEventListener('click', () => {
+      const collapsibleContent = btn.nextElementSibling;
+      btn.classList.toggle('active');
+
+      if (btn.classList.contains('active')) {
+        collapsibleContent.style.maxHeight =
+          collapsibleContent.scrollHeight + 'rem';
+      } else {
+        collapsibleContent.style.maxHeight = 0;
+      }
+    })
+  );
+};
