11a507e48e7737d794b400662b722f37db56c1d7
HelixW <shreyas.2000@hotmail.com>
2020-02-14 10:37:32 +0530

    Responsive fixes

--- src/js/main.js
+++ src/js/main.js
@@ -218,6 +218,27 @@ const SPONSOR_DATA = [
   new sponsor('./images/sponsors/hasura.png', 'https://hasura.io/')
 ];
 
+document.addEventListener('DOMContentLoaded', function () {
+    let devfolioOptions = {
+        buttonSelector: '#devfolio-apply-now',
+        key: 'code2create',
+    }
+
+    let script = document.createElement('script');
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+
+    script.onload = function () {
+        new Devfolio(devfolioOptions);
+    }
+
+    script.onerror = function () {
+        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function () {
+            window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
+        });
+    }
+});
+
 window.onload = () => {
   // Navbar Scrolling
   window.onscroll = () => {
